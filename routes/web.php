93ef0f8c63e2ecaa663ce8ee50984a239f665ffb
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-12-27 19:22:05 +0100

    create new controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +22,9 @@ Route::get('/', function () {
 Route::get('/tareas',[TodosController::class,'index'])->name('todos');
 
 Route::post('/tareas',[TodosController::class,'store'])->name('todos'); 
-
+    
 Route::get('/tareas{id}',[TodosController::class,'show'])->name('todos-edit'); 
 Route::patch('/tareas{id}',[TodosController::class,'update'])->name('todos-update'); 
 Route::delete('/tareas{id}',[TodosController::class,'destroy'])->name('todos-destroy'); 
+
+Route::resource('categories', CategoriesController::class);
